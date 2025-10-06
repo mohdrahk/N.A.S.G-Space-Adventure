4c56d5bf804fa8ff8fb9c86b8e756d41ed51d82b
mohdrahk <mohammedrak@gmail.com>
2025-10-07 01:42:34 +0300

    switched from ID to classes as the game was breaking due to classes

--- script backup.js
+++ script backup.js
@@ -11,55 +11,56 @@ let direction = 1
 // Drawing 400 cells
 for (let i = 0; i < width * width; i++) {
   const square = document.createElement("div")
+  square.className = "cell"
   grid.appendChild(square)
 }
 
 const squares = Array.from(document.querySelectorAll(".grid div"))
 console.log(squares)
 
-// Placing the Alien Invaders
+//Placing the Alien Invaders
 const alienInvaders = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 21, 22, 23, 24, 25,
   26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 40, 41, 42, 43, 44, 45, 46, 47, 48,
   49, 50, 51, 52, 53, 54, 55,
 ]
 
-// Drawing the invaders on top of the grid
+//Drawing the invaders ontop of the grid
 function draw() {
   for (let i = 0; i < alienInvaders.length; i++) {
     if (!alienRemoved.includes(i)) {
-      squares[alienInvaders[i]].classList.add("invader")
+      squares[alienInvaders[i]].setAttribute("id", "invader")
     }
   }
 }
 
-squares[currentShooterIndex].classList.add("shooter")
+squares[currentShooterIndex].setAttribute("id", "shooter")
 
-// Redrawing the Aliens
+//Redrawing the Aliens
 function remove() {
   for (let i = 0; i < alienInvaders.length; i++) {
-    squares[alienInvaders[i]].classList.remove("invader")
+    squares[alienInvaders[i]].removeAttribute("id")
   }
 }
 
-// Logic for Moving the shooter
+//Logic for Moving the shooter
 function moveShooter(e) {
-  squares[currentShooterIndex].classList.remove("shooter")
+  squares[currentShooterIndex].removeAttribute("id")
   switch (e.key) {
     case "ArrowLeft":
-      if (currentShooterIndex % width !== 0) currentShooterIndex -= 1 // Will not move beyond the left border
+      if (currentShooterIndex % width !== 0) currentShooterIndex -= 1 //Will not move to beyond the left border
       break
     case "ArrowRight":
-      if (currentShooterIndex % width < width - 1) currentShooterIndex += 1 // Will not move beyond the right border
+      if (currentShooterIndex % width < width - 1) currentShooterIndex += 1 //Will not move to beyond the right border
       break
   }
-  squares[currentShooterIndex].classList.add("shooter")
+  squares[currentShooterIndex].setAttribute("id", "shooter")
 }
 
-// Event listener to move Hero Ship
+//Event listener to move Hero Ship
 document.addEventListener("keydown", moveShooter)
 
-// Logic to move Aliens by redrawing them with increments
+//Logic to move Aliens by redrawing them with increments
 function moveInvaders() {
   const leftEdge = alienInvaders[0] % width === 0
   const rightEdge =
@@ -104,31 +105,36 @@ function moveInvaders() {
 }
 
 draw()
+// Check for game over condition
+if (alienInvaders.some((invader) => invader >= squares.length - width)) {
+  resultsDisplay.innerHTML = "GAME OVER"
+  clearInterval(invadersId)
+}
 
-// Speed of invaders movement
+//Speed of invaders movement
 invadersId = setInterval(moveInvaders, 600)
 
-// Adding bullets/lasers
+//Adding bullets/lasers
+
 function shoot(e) {
   let bulletId
   let currentLaserIndex = currentShooterIndex
 
   function moveBullet() {
-    // Remove the bullet class only if it's not the shooter's position
-    if (squares[currentLaserIndex].classList.contains("bullet")) {
-      squares[currentLaserIndex].classList.remove("bullet") // Remove bullet class
+    // Remove the bullet ID only if it's not the shooter's position
+    if (squares[currentLaserIndex].id === "bullet") {
+      squares[currentLaserIndex].removeAttribute("id") // Remove bullet ID
     }
 
     currentLaserIndex -= width // Move the bullet up
 
-    // Add the bullet class to the new position
+    // Add the bullet ID to the new position
     if (currentLaserIndex >= 0) {
-      squares[currentLaserIndex].classList.add("bullet")
+      squares[currentLaserIndex].setAttribute("id", "bullet")
     }
     if (squares[currentLaserIndex].classList.contains("invader")) {
-      squares[currentLaserIndex].classList.remove("bullet")
+      squares[currentLaserIndex].classList.remove("laser")
       squares[currentLaserIndex].classList.remove("invader")
-      alienRemoved.push(alienInvaders.indexOf(currentLaserIndex)) // Mark invader as removed
     }
   }
 
@@ -139,7 +145,7 @@ function shoot(e) {
   }
 }
 
-// Event to shoot
+//Event to shoot
 document.addEventListener("keydown", shoot)
 
 draw()
